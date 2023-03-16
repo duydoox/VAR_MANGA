226d797c0d65ef96040fa3a5dddc477c66e27645
duy <duydq2k1@gmail.com>
2023-03-16 14:47:33 +0700

    init

---
+++ src/Components/Brand.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Image } from 'react-native'
+import { useTheme } from '@/Hooks'
+
+type Props = {
+  height?: number | string
+  width?: number | string
+  mode?: 'contain' | 'cover' | 'stretch' | 'repeat' | 'center'
+}
+
+const Brand = ({ height, width, mode }: Props) => {
+  const { Layout, Images } = useTheme()
+
+  return <View style={{ height, width }} />
+}
+
+Brand.defaultProps = {
+  height: 200,
+  width: 200,
+  mode: 'contain',
+}
+
+export default Brand

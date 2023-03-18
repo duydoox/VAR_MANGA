9f0471e99f2052f701e0d9fa35444772a7a79e05
duy <duydq2k1@gmail.com>
2023-03-18 16:28:02 +0700

    add splash screen and config build release

--- src/Containers/StartupContainer.tsx
+++ src/Containers/StartupContainer.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
-import { ActivityIndicator, View, Text } from 'react-native'
+import { ActivityIndicator, View, Text, ImageBackground } from 'react-native'
 import { useTranslation } from 'react-i18next'
 import { useTheme } from '@/Hooks'
 import { setDefaultTheme } from '@/Store/Theme'
 import { navigateAndSimpleReset } from '@/Navigators/utils'
 
 const StartupContainer = () => {
-  const { Layout, Gutters, Fonts } = useTheme()
+  const { Layout, Gutters, Fonts, Images } = useTheme()
 
   const { t } = useTranslation()
 
@@ -14,7 +14,7 @@ const StartupContainer = () => {
     await new Promise(resolve =>
       setTimeout(() => {
         resolve(true)
-      }, 100),
+      }, 10000),
     )
     await setDefaultTheme({ theme: 'default', darkMode: null })
     navigateAndSimpleReset('Main')
@@ -25,10 +25,12 @@ const StartupContainer = () => {
   })
 
   return (
-    <View style={[Layout.fill, Layout.colCenter, { backgroundColor: 'black' }]}>
-      <ActivityIndicator size={'large'} style={[Gutters.largeVMargin]} />
-      <Text style={Fonts.textCenter}>{t('welcome')}</Text>
-    </View>
+    <ImageBackground source={Images.splash_image} style={[Layout.fill]}>
+      <View style={[Layout.fill, Layout.colCenter]}>
+        <ActivityIndicator size={'large'} style={[Gutters.largeVMargin]} />
+        <Text style={Fonts.textCenter}>{t('welcome')}</Text>
+      </View>
+    </ImageBackground>
   )
 }
 
--- src/Theme/Images.ts
+++ src/Theme/Images.ts
@@ -3,5 +3,6 @@ import { ThemeVariables } from './theme'
 export default function ({}: ThemeVariables) {
   return {
     logo: require('@/Assets/Images/TOM.png'),
+    splash_image: require('@/Assets/Images/splash_image.jpg'),
   }
 }
